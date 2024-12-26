60deca8c2821e72e62b047d56080ca7de9e5f754
Anderson Filho <filhoando@gmail.com>
2024-12-26 20:26:20 -0300

    parando na aula 158

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,13 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Position p = new Position(5, 5);
-		System.out.println(p);
-
+		Board b = new Board(2, 4);
 	}
 
 }
