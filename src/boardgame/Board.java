08b611177df2ad49e637cb01ddca9b4dba4f8406
Anderson Filho <filhoando@gmail.com>
2025-01-06 18:51:02 -0300

    aula 164

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -47,17 +47,16 @@ public class Board {
 		}
 		
 		public Piece removePiece(Position position) {
-			if(!positionExists(position)) {
-				throw new BoardException("Essa posição não existe no tabuleiro");
+			if (!positionExists(position)) {
+				throw new BoardException("Position not on the board");
 			}
-			if (piece(position)== null) {
+			if (piece(position) == null) {
 				return null;
 			}
 			Piece aux = piece(position);
 			aux.position = null;
-			pieces[position.getRow()]{position.getColumn()] = null;
+			pieces[position.getRow()][position.getColumn()] = null;
 			return aux;
-			}
 		}
 		
 		private boolean positionExists(int row, int column) {
